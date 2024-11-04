7ce554090f76dda91378fda1e6221894edf6e694
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2024-11-04 13:25:00 -0500

    feat: update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
 import "./App.css";
-import mail from "./assets/mail.svg";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 import github from "./assets/github.svg";
 import linkedin from "./assets/linkedin.svg";
+import mail from "./assets/mail.svg";
 
 function App() {
   return (
-    <>
-      <div className="container-custome">
-        <h1>Hey, it&apos;s Franco Vasquez</h1>
-        <div className="subtitle-container">
-          <p>
-            Software Engineering Student @{" "}
-            <a href="https://www.upc.edu.pe/">UPC</a>
-          </p>
-          <p>
-            Cloud & Automation Intern @{" "}
-            <a href="https://www.entel.pe/">Entel Perú</a>
-          </p>
-        </div>
-        <div className="logos-container">
-          <a href="mailto:vasquezemmanuel439@gmail.com">
-            <img src={mail} />
-          </a>
-          <a href="https://github.com/FrancoEVP10">
-            <img src={github} />
-          </a>
-          <a href="https://www.linkedin.com/in/franco-vasquez-poma-435190211/">
-            <img src={linkedin} />
-          </a>
-        </div>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={Home(mail, github, linkedin)} />
+    </Routes>
   );
 }
 
